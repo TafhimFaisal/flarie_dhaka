f495b03c25fe44b5c2a93035168ba0476c460991
TafhimFaisal <tropical10tafhim@gmail.com>
2023-12-03 00:51:39 +0600

    responsive

--- swe-frontend-1-main/src/components/game-box/GameBox.tsx
+++ swe-frontend-1-main/src/components/game-box/GameBox.tsx
@@ -1,10 +1,30 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode,useEffect, useState } from 'react';
 import { ContainerStyled } from './styled';
 import { TestElement } from '../../enums/TestElement';
+import { gameConfig } from '../../config/game-config';
 
 interface Props {
   children: ReactNode;
 }
 export const GameBox = ({ children }: Props) => {
-  return <ContainerStyled data-testid={TestElement.GAME_BOX}>{children}</ContainerStyled>;
+  const [screenSize, setScreenSize] = useState({ width: gameConfig.container.maxWidth, height: gameConfig.container.maxHeight }),
+        handleResize = () => {
+          setScreenSize({ 
+            width: window.innerWidth <= gameConfig.container.maxWidth ? window.innerWidth - 20 : gameConfig.container.maxWidth, 
+            height: window.innerHeight <= gameConfig.container.maxHeight ? window.innerHeight - 20 : gameConfig.container.maxHeight 
+          })
+        }
+  useEffect(() => {
+    setScreenSize({ 
+      width: window.innerWidth <= gameConfig.container.maxWidth ? window.innerWidth - 20 : gameConfig.container.maxWidth, 
+      height: window.innerHeight <= gameConfig.container.maxHeight ? window.innerHeight - 20 : gameConfig.container.maxHeight 
+    })
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
+
+  return <ContainerStyled data-testid={TestElement.GAME_BOX} style={{width:screenSize.width,height:screenSize.height}}>{children}</ContainerStyled>;
 };
